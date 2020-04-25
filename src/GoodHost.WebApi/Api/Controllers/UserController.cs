7da1f521a820c641a8acef2a29434470f8e818f0
Zerina Đuherić <zerinadjuheric@gmail.com>
2020-04-25 02:31:23 +0200

    Requests

---
+++ src/GoodHost.WebApi/Api/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Abp.WebApi.Controllers;
+using GoodHost.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GoodHost.Api.Controllers
+{
+    public class UserController : AbpApiController
+    {
+        /// <summary>
+        /// Returns all movies.
+        /// </summary>
+        /// <returns>A JSON list of all movies.</returns>
+        ///[Route("users/all")]
+        [HttpGet]
+        public IHttpActionResult All()
+        {
+            List<User> users = new List<User>()
+        {
+            new User()
+            {
+                Id = 5,
+                Name = "Mujo",
+                Surname = "Mujic",
+                UserName = "mmujic",
+                EmailAddress = "test",
+            },
+            new User()
+            {
+                Id = 6,
+                Name = "Selma",
+                Surname = "Skopljakovic",
+                UserName = "selma",
+                EmailAddress = "test1"
+            },
+            new User()
+            {
+                Id = 7,
+                Name = "Sejla",
+                Surname = "Selimovic",
+                UserName = "sejlas",
+                EmailAddress = "test2"
+            }
+        };
+
+            return Ok(users);
+        }
+    }
+}
